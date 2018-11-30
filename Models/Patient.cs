a24156d4800872a4c3841cd9a67004d857fdf780
Wadkar Satish <satish.wadkar@tieto.com>
2018-11-30 10:40:37 +0530

    Merge message chat bot code
    Merge message chat bot code and updated required packages

---
+++ Models/Patient.cs
@@ -0,0 +1,9 @@
+﻿namespace EmergencyServicesBot.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public string SSN { get; set; }
+    }
+}
